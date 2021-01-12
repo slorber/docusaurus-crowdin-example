b03332df72df9acdfceec347349272fbc2368efe
slorber <lorber.sebastien@gmail.com>
2021-01-12 18:51:12 +0100

    master -> main

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -104,13 +104,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/slorber/docusaurus-crowdin-example/edit/master/website/',
+            'https://github.com/slorber/docusaurus-crowdin-example/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/slorber/docusaurus-crowdin-example/edit/master/website/blog/',
+            'https://github.com/slorber/docusaurus-crowdin-example/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
